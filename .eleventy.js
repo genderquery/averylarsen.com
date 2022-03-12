230747cef6abd0b9e293c2981f05e23bb457b0e9
Avery Larsen <hello@averylarsen.com>
2022-03-12 00:16:08 -0800

    Add post date
    - Add filters for formatting dates
    - Add date to post template

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.ignores.add("README.md");
 
@@ -9,5 +11,15 @@ module.exports = function (eleventyConfig) {
   // pretty print objects for debugging
   eleventyConfig.addFilter("pretty", function (value) {
     return JSON.stringify(value, null, 4);
+  });
+
+  eleventyConfig.addFilter("localeDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toLocaleString(
+      DateTime.DATE_MED
+    );
+  });
+
+  eleventyConfig.addFilter("isoDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toISODate();
   });
 };
